047dad5d4cd80e463990ec26c3f792152b8cbd57
pepilloelgrillo19 <157523378+pepilloelgrillo19@users.noreply.github.com>
2024-04-10 17:29:11 +0200

    Initial Commit

---
+++ 240410 Contadores/_40410_ContadoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  _40410_ContadoresApp.swift
+//  240410 Contadores
+//
+//  Created by Luis on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct _40410_ContadoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
